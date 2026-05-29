9e5b4dd6c715a300fb1cc0c4d77aba836bfcf42d
lgick <lgick@yandex.ru>
2026-05-29 11:18:16 +0300

    nvim-tree images

--- lua/paperblue.lua
+++ lua/paperblue.lua
@@ -338,6 +338,7 @@ local function set_highlights()
     NvimTreeCopiedHL = { bg = C.light_yellow, fg = C.carbon, nocombine = true },
     NvimTreeCutHL = { bg = C.light_orange, fg = C.carbon, nocombine = true },
 
+    NvimTreeImageFile = { fg = C.dark_grey, nocombine = true },
     NvimTreeIndentMarker = { fg = C.light_grey, nocombine = true },
 
     -- ==========================================
